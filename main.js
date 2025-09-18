abf7e893ea05256c472bb450f943f4953b49beb8
maptiva <maptiva.sig.app@gmail.com>
2025-09-18 11:11:04 -0300

    Nombres clientes en negritas

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ onSnapshot(collection(db, 'clientes'), (snapshot) => {
           clientCard.className = "bg-white p-4 rounded-lg shadow-md w-full flex justify-between items-center";
                     
           const clientNameSpan = document.createElement("span");
-          clientNameSpan.className = "cursor-pointer flex-grow";
+          clientNameSpan.className = "cursor-pointer flex-grow font-bold";
           clientNameSpan.textContent = client.nombre;
           clientNameSpan.dataset.id = doc.id; // The name is the main clickable area
                     
@@ -515,7 +515,7 @@ onSnapshot(collection(db, 'notas'), (snapshot) => {
         actionsContainer.className = 'flex justify-end space-x-3 mt-4';
 
         const editBtn = document.createElement('button');
-        editBtn.className = 'edit-nota-btn ml-4 p-1 rounded-full hover:bg-blue-100';
+        editBtn.className = 'edit-nota-btn p-1 rounded-full hover:bg-blue-100';
         editBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" /><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" /></svg>`;
 
         const deleteBtn = document.createElement('button');
